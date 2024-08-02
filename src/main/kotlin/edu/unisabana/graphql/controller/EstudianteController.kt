dd06f0c0612588bdd9dabecf8205502ce5f362b1
Sergio Torres <sdtorresl@unal.edu.co>
2024-08-01 23:26:11 -0500

    Get Estudiante by ID

--- src/main/kotlin/edu/unisabana/graphql/controller/EstudianteController.kt
+++ src/main/kotlin/edu/unisabana/graphql/controller/EstudianteController.kt
@@ -2,6 +2,7 @@ package edu.unisabana.graphql.controller
 
 import edu.unisabana.graphql.model.EstudianteDTO
 import edu.unisabana.graphql.service.EstudianteService
+import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
@@ -14,4 +15,7 @@ class EstudianteController(
     fun getAllEstudiantes(): List<EstudianteDTO> =
         estudianteService.findAll()
 
+    @QueryMapping
+    fun estudiante(@Argument id: Int): EstudianteDTO =
+        estudianteService.get(id)
 }
